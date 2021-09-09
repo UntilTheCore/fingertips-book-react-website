9c460313eee7be55ea878b4c786d8d61409bb64b
untilthecore <814898862@qq.com>
2021-09-09 11:39:54 +0800

    deploy

---
+++ precache-manifest.0581e57088dae89ae4972b5587971c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c6bba6e453b1a1bf1e0575e298926c",
+    "url": "/fingertips-book-react-website/index.html"
+  },
+  {
+    "revision": "e04e6d3451c52db4a77a",
+    "url": "/fingertips-book-react-website/static/css/main.b73e7102.chunk.css"
+  },
+  {
+    "revision": "4e52c2c389683a310e3e",
+    "url": "/fingertips-book-react-website/static/js/2.8b1e5add.chunk.js"
+  },
+  {
+    "revision": "fc8b115d4064d6797b6def218d457695",
+    "url": "/fingertips-book-react-website/static/js/2.8b1e5add.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04e6d3451c52db4a77a",
+    "url": "/fingertips-book-react-website/static/js/main.06778889.chunk.js"
+  },
+  {
+    "revision": "92b1679a1ca8e4f249a5",
+    "url": "/fingertips-book-react-website/static/js/runtime-main.a85067b5.js"
+  }
+]);
